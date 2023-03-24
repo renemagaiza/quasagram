d7489698cc05fc7311ccf5f27e713b8cf888da35
Rene Magaiza <83861035+renemagaiza@users.noreply.github.com>
2023-03-24 16:28:53 +0100

    Initial commit

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-page class="constrain q-pa-md bg-grey-5">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <templete v-if="!loadingPosts && posts.length">
+          <q-card 
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md" 
+            flat 
+            bordered>
+            <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">Rene__Magaiza</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+            </q-item>
+
+            <q-separator vertical />
+
+            <q-img
+            :src="post.imageUrl"
+            />
+
+            <q-card-section>
+            <div >{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate}}</div>
+            </q-card-section>
+          </q-card>
+        </templete>
+        <template v-else>
+          <q-card flat bordered >
+           <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" size="40px"/>
+              </q-item-section>
+
+              <q-item-section>
+                  <q-item-label>
+                    <q-skeleton type="text" animation="fade" />
+                  </q-item-label>
+                  <q-item-label caption>
+                    <q-skeleton type="text" animation="fade" />
+                  </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton type="text" width="50%" class="text-subtitle2" animation="fade" />
+            </q-card-section>
+          </q-card>
+        </template>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+        <q-item-section avatar>
+          <q-avatar size="48px">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Rene__Magaiza</q-item-label>
+          <q-item-label caption>
+            Rene Magaiza
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      </div>
+    </div>
+   
+  </q-page>
+</template>
+
+<script>
+import { date, Loading } from 'quasar'
+
+export default {
+  name: 'PageHome',
+  data(){
+    return{
+      posts: [],
+      loadingPosts: false
+    }
+  },
+  methods: {
+    getPosts(){
+      this.loadingPosts = true
+      this.$axios.get(`${process.env.API}/posts`).then(response => {
+        this.posts = response.data  
+        this.loadingPosts = false
+    }).catch(err => {
+      this.$q.dialog({
+      title: 'Error',
+      message: 'Could not download posts.'
+    })
+      this.loadingPosts = false
+    })
+
+    }
+  },
+  filters: {
+    niceDate(value){
+      return  date.formatDate(value, 'MMM D h:mmA')
+    }
+  },
+  created(){
+    this.getPosts()
+  }
+}
+</script>
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+
+</style>
